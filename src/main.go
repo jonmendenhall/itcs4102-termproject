0fc72cc42912e6a0c17e78f60a2231481de84233
Jon Mendenhall <jmenden4@uncc.edu>
2022-04-04 19:19:51 -0400

    add start of main and overview

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("ITCS-4102 Term Project: Mountain Map")
+	fmt.Println()
+
+	// overall process:
+	// - generate starting map using layers of perlin noise
+	// - run erosion simulation to update terrain
+	// - make path between 2 points on map
+}
